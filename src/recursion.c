aaf8ca4304f29de5063228a7e958c790b41c8783
nisanurkorapp <nisanur.korap@ibu.edu.mk>
2026-03-13 23:35:25 +0000

    i finished my homework

--- src/recursion.c
+++ src/recursion.c
@@ -17,8 +17,38 @@ Rules:
 - You must create a recursive function.
 - You cannot use a loop to print rows.
 - Base case must be correct.
-
-sjskks
-lotrrr
-xjkxk
 */
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+    int height;
+
+    // Get height of pyramid
+    printf("Height: ");
+    scanf("%d", &height);
+
+    // Draw pyramid
+    draw(height);
+}
+
+void draw(int n)
+{
+    // If nothing to draw
+    if (n <= 0)
+    {
+        return;
+    }
+
+    // Draw pyramid of height n - 1
+    draw(n - 1);
+
+    // Draw one more row of width n
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
